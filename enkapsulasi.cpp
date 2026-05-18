da2a0d6eff95e35f1c5ddc63af8b0f50cd4af50d
zaki_mubaarok <zakimubaarok918@gmail.com>
2026-05-18 10:55:37 +0700

    Implement main function to demonstrate remoteLampu class functionality

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -12,3 +12,13 @@ public:
         return sakalarNo[i];
     }
 };    
+
+int main() {
+    remoteLampu lampuRumah;
+
+    lampuRumah.setSaklarNo(0, "Lampu Teras Rumah");
+    lampuRumah.setSaklarNo(1, "Lampu Ruang Tamu");
+    lampuRumah.setSaklarNo(2, "Lampu Kamar Tidur");
+    lampuRumah.setSaklarNo(3, "Lampu Dapur");
+
+}
