f2fe3afb3a1dedff42eb5d0352721aada8b02653
zaki_mubaarok <zakimubaarok918@gmail.com>
2026-05-18 11:12:51 +0700

    Add abstract class 'seseorang' with virtual method 'pesan' in polimorpi.cpp

--- polimorpi.cpp
+++ polimorpi.cpp
@@ -1,2 +1,10 @@
 #include <iostream>
 using namespace std;
+
+class seseorang {
+public:
+    virtual void pesan() = 0;
+    // virtual void pesan() {
+    //     cout << "Hallo saya seseorang" << endl;
+    // }
+};
