d68aa2ba4f70e82ce5d2474f691241f953a2ee77
zaki_mubaarok <zakimubaarok918@gmail.com>
2026-05-18 10:26:24 +0700

    Implement AbstraksiKlas with private members and setXY method

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
 using namespace std;
 
+class AbstraksiKlas {
+private: string x,y;
+
+public:
+
+    // method untuk mengisi nilai
+    // private member
+    void setXY(string a, string b) {
+        x = a;
+        y = b;
+    }
