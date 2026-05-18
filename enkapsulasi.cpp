e0ca689e2a3f25a9a0a237c80d563b5d245ae286
zaki_mubaarok <zakimubaarok918@gmail.com>
2026-05-18 10:56:43 +0700

    Fix main function to correctly display saklarNo values from remoteLampu class

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -21,4 +21,10 @@ int main() {
     lampuRumah.setSaklarNo(2, "Lampu Kamar Tidur");
     lampuRumah.setSaklarNo(3, "Lampu Dapur");
 
+    cout << lampuRumah.getSaklarNo(0) << endl;
+    cout << lampuRumah.getSaklarNo(1) << endl;
+    cout << lampuRumah.getSaklarNo(2) << endl;
+    cout << lampuRumah.getSaklarNo(3) << endl;
+
+    return 0;
 }
