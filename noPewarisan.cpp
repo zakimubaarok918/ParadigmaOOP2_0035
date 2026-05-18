7e06b2e62d0356854f7111b40eff373bc22cc6a6
zaki_mubaarok <zakimubaarok918@gmail.com>
2026-05-18 10:43:19 +0700

    Add baseClass and baseClass1 implementations with perkenalan method in noPewarisan.cpp

--- noPewarisan.cpp
+++ noPewarisan.cpp
@@ -1,3 +1,15 @@
 #include <iostream>
 using namespace std;
 
+class baseClass final{
+public:
+    virtual void perkenalan() {
+        cout << "Hallo saya Function dari base class";
+    }
+};
+class baseClass1 {
+public:
+    virtual void perkenalan() {
+        cout << "Hallo saya Function dari base class";
+    }
+};
