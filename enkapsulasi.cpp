83ec9aec2d0cd6a31a26d41cb0869be45a781216
zaki_mubaarok <zakimubaarok918@gmail.com>
2026-05-18 10:52:10 +0700

    Add remoteLampu class with getter and setter for sakalarNo

--- enkapsulasi.cpp
+++ enkapsulasi.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
 using namespace std;
 
+class remoteLampu {
+private:
+    string sakalarNo[10];
+public:
+    void setSakalarNo(int i, string value) {
+        sakalarNo[i] = value;
+    }
+    string getSakalarNo(int i) {
+        return sakalarNo[i];
+    }
+};    
